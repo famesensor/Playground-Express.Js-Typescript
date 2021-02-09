199471f2fbb8cddf7b36ff345fbb1890763edd78
famesensor <sathit.ju@outlook.com>
2021-02-09 22:16:22 +0700

    :hammer: setup todo route

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
+import {
+    createTodo,
+    getTodos,
+    updateTodo,
+    deleteTodo
+} from '../controller/todos';
 const router = Router();
 
-router.route('/').post();
+router.route('/').post(createTodo).get(getTodos);
 
-router.route('/:id').get().patch().delete();
+router.route('/:id').get().patch(updateTodo).delete(deleteTodo);
 
 export default router;

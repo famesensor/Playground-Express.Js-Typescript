7570fd4af6e68a2c5e41f54f74afa89e4b9a9464
famesensor <sathit.ju@outlook.com>
2021-02-09 22:15:45 +0700

    :sparkles: new function create, get, update and delete

--- src/controller/todos.ts
+++ src/controller/todos.ts
@@ -1,3 +1,48 @@
 import { RequestHandler } from 'express';
 
-export const createTodo: RequestHandler = (req, res, next) => {};
+import { Todo } from '../models/todo';
+
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+    const text = (req.body as { text: string }).text;
+    const newTodo = new Todo(Math.random().toString(), text);
+
+    TODOS.push(newTodo);
+    console.log(TODOS);
+    res.status(201).json({ success: true, data: newTodo });
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+    res.status(200).json({ success: true, todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+    const todoId = req.params.id;
+    console.log(todoId);
+    const updatedText = (req.body as { text: string }).text;
+
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+    if (todoIndex < 0) {
+        throw new Error('Counld not find todo!');
+    }
+
+    TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+    res.status(200).json({ success: true, data: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+    const todoId = req.params.id;
+
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+    if (todoIndex < 0) {
+        throw new Error('Counld not find todo!');
+    }
+
+    TODOS.splice(todoIndex, 1);
+
+    res.status(200).json({ success: true, data: `${todoId} is deleted` });
+};

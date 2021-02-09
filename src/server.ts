6541a8eef2939e2b8a23ecfc3d05de2f92a848cb
famesensor <sathit.ju@outlook.com>
2021-02-09 22:14:48 +0700

    :hammer: set middleware json

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import todoRoutes from './routes/todo';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/todos', todoRoutes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {

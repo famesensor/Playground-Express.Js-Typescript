e1c3ae50c9f178c5629e010689ba8569b14eedec
famesensor <sathit.ju@outlook.com>
2021-02-21 15:15:43 +0700

    :hammer: delete code and add email columm

--- src/models/user.ts
+++ src/models/user.ts
@@ -11,27 +11,12 @@ const UserSchema: Schema = new Schema({
         type: String,
         required: true
     },
+    email: {
+        type: String,
+        required: true
+    },
     createdAt: {
         type: Date,
         default: Date.now
     }
 });
-
-interface IUser extends Document {
-    username: String;
-    password: String;
-}
-
-interface IUserModel extends IUser {
-    macthPassword(password: String): Promise<boolean>;
-}
-
-UserSchema.pre<IUserModel>('save', async function (next): Promise<void> {
-    if (!this.isModified('password')) {
-        next();
-    }
-    const salt: String = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-});
-
-const userModel = model<IUserModel>('User', UserSchema);

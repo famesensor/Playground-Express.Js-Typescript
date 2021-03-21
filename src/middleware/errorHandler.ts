151222ca079db276c698e71d9e4cf788389a03f7
famesensor <sathit.ju@outlook.com>
2021-03-21 23:34:23 +0700

    :tada: init commit error handler

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
+import ErrorReponse from '../utils/errorReponse/errorReponse';
+
+interface ErrorReponseExtend extends ErrorReponse {
+    code?: number;
+    message: string | any;
+    errors?: any;
+}
+
+const errorHandler = (
+    err: ErrorReponseExtend,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    let error = { ...err };
+
+    error.message = err.message;
+    console.log(err);
+
+    // mongoose validation error
+    if (err.name === 'ValidattionError') {
+        const errors: {
+            [key: string]: string;
+        } = {};
+        Object.values<{
+            [key: string]: string;
+        }>(err.errors).forEach((val) => (errors[val.path] = val.message));
+        error.message = errors; // new assigned way
+        error.statusCode = 400;
+    }
+
+    res.status(error.statusCode || 500).json({
+        status: 'failed',
+        message: error.message || `Internal server error`
+    });
+};

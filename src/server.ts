b60a31d8bf8cc873d55e0c58ea90aa793aaa497a
famesensor <sathit.ju@outlook.com>
2021-03-21 16:01:04 +0700

    :hammer: setup cors middlware and console port

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express';
+import * as cors from 'cors';
 
 import todoRoutes from './routes/todo';
 
 const app = express();
 
+app.use(cors.default());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -13,4 +15,8 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({ message: err.message });
 });
 
-app.listen(3000);
+const port: number = 3000;
+
+app.listen(port, () => {
+    console.log(`server running on port -> ${port}`);
+});

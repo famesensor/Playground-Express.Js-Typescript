757f195f336f1ac80a73354c9f60ff00e535dc2e
famesensor <sathit.ju@outlook.com>
2021-04-13 13:06:52 +0700

    [:hammer:]: add user interface

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,6 +1,15 @@
-export interface UserSignup {
+export interface IUser {
     username: string;
     password: string;
     email: string;
     picture: string;
+    role: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface UserSignup {
+    username: string;
+    password: string;
+    email: string;
 }

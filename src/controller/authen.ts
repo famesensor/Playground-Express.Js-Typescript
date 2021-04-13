f6f1ec8322124a89e762d4a267ce250e14974e96
famesensor <sathit.ju@outlook.com>
2021-04-13 16:09:26 +0700

    [:hammer:] fix signup and create signin and get profile

--- src/controller/authen.ts
+++ src/controller/authen.ts
@@ -5,15 +5,19 @@ import { Request, Response, NextFunction } from 'express';
 
 import { UserModel } from '../models/user';
 
-import { UserSignup } from '../interfaces/user';
+import { UserSignup, UserSignin } from '../interfaces/user';
 
 export const signupUser = asyncHandler(async function (req, res, next) {
     let { username, password, email }: UserSignup = req.body;
 
+    if (!username || !password || !email) {
+        return next(new ErrorHandler(`Validator error`, 400));
+    }
+
     const exists_user = UserModel.findOne({
         $or: [{ username: username }, { email: email }]
     });
-    if (exists_user) {
+    if (!exists_user) {
         return next(new ErrorHandler(`User already exist`, 400));
     }
 
@@ -28,5 +32,43 @@ export const signupUser = asyncHandler(async function (req, res, next) {
 
     res.status(201).json({
         status: 'success'
+    });
+});
+
+export const signInUser = asyncHandler(async (req, res, next) => {
+    let { username, password }: UserSignin = req.body;
+
+    if (!username || !password) {
+        return next(new ErrorHandler(`Validator error`, 400));
+    }
+
+    const user = await UserModel.findOne({ username: username }).select(
+        '+password'
+    );
+    if (!user) {
+        return next(new ErrorHandler(`User not found`, 404));
+    }
+
+    if (!(await user.matchPassword(password))) {
+        return next(new ErrorHandler(`User not found`, 400));
+    }
+
+    let token = user.getToken();
+
+    res.status(200).json({
+        status: 'success',
+        token: token
+    });
+});
+
+export const getProfile = asyncHandler(async function (req, res, next) {
+    let username = req.user?.username;
+    let user = await UserModel.findOne({ username });
+    if (!user) {
+        return next(new ErrorHandler(`User not found`, 404));
+    }
+    res.status(200).json({
+        status: 'success',
+        data: user
     });
 });

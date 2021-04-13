67c1f3116dd897cf87f3fa339bf1f26ec9bcf189
famesensor <sathit.ju@outlook.com>
2021-04-13 16:08:58 +0700

    [:hammer:] setup connect db

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,46 @@
-import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+import express from 'express';
 import * as cors from 'cors';
 import errorHandler from './middleware/errorHandler';
+import mongoose from 'mongoose';
 
-import todoRoutes from './routes/todo';
 import authRoutes from './routes/authen';
 
-const app = express();
+dotenv.config();
+// init express
+const app: express.Express = express();
 
+// connect db
+let mongodbURL: string = process.env.MONGODB_URL!;
+mongoose
+    .connect(mongodbURL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log(`mongodb coonect success`);
+    })
+    .catch((err) => {
+        console.log(
+            `MongoDB connection error. Please make sure MongoDB is running. ${err}`
+        );
+    });
+
+// middleware
 app.use(cors.default());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// init route path
 app.use('/api/auth/', authRoutes);
-app.use('/api/todos', todoRoutes);
 
 app.use(errorHandler);
 
-const port: number = 3000;
+const port: number = 3000 || process.env.PORT;
 
+// port run
 app.listen(port, () => {
     console.log(`server running on port -> ${port}`);
 });
